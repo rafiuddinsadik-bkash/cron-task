8f429fc615e48b2a46ce049e40b546ba934bf569
rafiuddinsadik-bkash <mohammad.sadique@bkash.com>
2022-12-26 15:25:37 +0600

    igw added in routes

--- routes.tf
+++ routes.tf
@@ -1,17 +1,23 @@
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_default_vpc.default.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
 resource "aws_route_table" "public" {
   vpc_id = aws_default_vpc.default.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
+    gateway_id = aws_internet_gateway.gw.id
   }
 
   tags = {
     Name = "public"
   }
 }
-
-
 
 resource "aws_route_table_association" "public_us_east_1a" {
   subnet_id      = aws_default_subnet.default_az1.id
