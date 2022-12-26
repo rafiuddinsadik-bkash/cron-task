0d49bfcc40c95c1551e9d3e51f64fb278284daba
rafiuddinsadik-bkash <mohammad.sadique@bkash.com>
2022-12-26 15:06:42 +0600

    bugfix

--- variables.tf
+++ variables.tf
@@ -1,13 +1,13 @@
 #Vars
 
 variable "region" {
-  value = "us-east-1"
+  default = "us-east-1"
 }
 
 variable "instance_type" {
-  value = "t2.micro"
+  default = "t2.micro"
 }
 
 variable "ec2-name" {
-  value = "Demo"
+  default = "Demo"
 }
