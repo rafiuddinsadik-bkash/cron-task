f03109d2be06ee7689f109e38af45c5db51e15f2
rafiuddinsadik-bkash <mohammad.sadique@bkash.com>
2022-12-27 11:59:05 +0600

    bugfix

--- main.tf
+++ main.tf
@@ -6,9 +6,9 @@ resource "aws_instance" "ec2_demo" {
 
   ami  = "ami-0b5eea76982371e91" #amazon linux 2
   instance_type = var.instance_type
-  iam_instance_profile = aws_iam_instance_profile.iam_profile.name
+  iam_instance_profile = aws_iam_instance_profile.cron_inst_profile.name
   
-  subnet_id = aws_default_subnet.default_az1.id
+  subnet_id = aws_default_subnet.def_subnet.id
 
   root_block_device {
     volume_type = "gp2"
