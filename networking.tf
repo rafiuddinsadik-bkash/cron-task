e11be11748e69df2fb31e6b19ace2bf886b0e9b7
rafiuddinsadik-bkash <mohammad.sadique@bkash.com>
2022-12-28 12:45:17 +0600

    ansible cp file

--- networking.tf
+++ networking.tf
@@ -33,12 +33,4 @@ resource "aws_security_group" "ec2-SG" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  egress {
-    description = "output from webserver"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
